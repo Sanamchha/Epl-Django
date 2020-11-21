0b5f3584877780d32eb11599e5505ae4bcccbc1e
Sanamchha <52907119+Sanamchha@users.noreply.github.com>
2020-11-21 14:55:22 +0545

    Add files via upload

--- epl/epl/urls.py
+++ epl/epl/urls.py
@@ -17,10 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from epl_webapp import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('^$',views.index,name='Homepage'), 
-    url('predictor',views.predictor,name='predictor'), 
+    url('predictor',views.predictor,name='predictor'),
+    url('trial',views.trial,name='trial'), 
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
